e5f6aec6cf086004962c6814ca2f96954a4a88db
Aatraya23 <royaatraya7@gmail.com>
2024-02-27 18:59:15 +0530

    changes

---
+++ Medical-Asst-System/app/Models/medical_supplies_medical.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class medical_supplies_medical extends Model
+{
+    use HasFactory;
+    protected $fillable=
+    [
+        'product_id',
+        'product_name',
+        'product_keywords',
+        'product_rate',
+        'product_image',
+        'product_para',
+        'product_desc',
+        'product_makers'
+    ];
+}
