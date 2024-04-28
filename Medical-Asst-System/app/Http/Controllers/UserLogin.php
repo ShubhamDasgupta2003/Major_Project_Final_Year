21658ca1ded3c11d81f3e22a26b34f60a44ac5ba
rameshroy <rameshroyprl2019@gmail.com>
2024-04-28 19:15:44 +0530

    add user id and user email as a session variable

--- Medical-Asst-System/app/Http/Controllers/UserLogin.php
+++ Medical-Asst-System/app/Http/Controllers/UserLogin.php
@@ -13,6 +13,8 @@ class UserLogin extends Controller
     $user=User_info::where('user_email',$request->input('email'))->where('user_password',$request->input('password'))->first();
     if($user){
          $request->session()->put('user_name',$user->user_first_name);
+         $request->session()->put('user_id',$user->user_id);
+         $request->session()->put('user_email',$user->user_email);
          return redirect('/');
      }
      else{
