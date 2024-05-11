c3dc56fc2fd71cb7a38a4843d54c0bf39187b75d
rameshroy <rameshroyprl2019@gmail.com>
2024-05-11 10:25:45 +0530

    make user login redirection

--- Medical-Asst-System/app/Http/Controllers/UserLogin.php
+++ Medical-Asst-System/app/Http/Controllers/UserLogin.php
@@ -8,22 +8,21 @@ use Illuminate\Support\Facades\Session;
 
 class UserLogin extends Controller
 {   public function index(){
-    session()->put('logged_in',0);
-    return view("user_login");
+    if (session()->has('user_name')) {
+        // Redirect to another page
+        return view('Blood_Booking/profilePage');
+    }{
+    return view("user_login");}
     
     }
 
     public function login(Request $request){
-        if (session()->has('logged_in')) {
-            // Redirect to another page
-            return view('Blood_Booking/profilePage');
-        }
+      
     $user=User_info::where('user_email',$request->input('email'))->where('user_password',$request->input('password'))->first();
     if($user){
          $request->session()->put('user_name',$user->user_first_name);
          $request->session()->put('user_id',$user->user_id);
          $request->session()->put('user_email',$user->user_email);
-         $request->session()->put('logged_in',1);
          return redirect('/');
      }
      else{
