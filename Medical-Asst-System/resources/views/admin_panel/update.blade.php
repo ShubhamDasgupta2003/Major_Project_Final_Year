b35068959a896079de8c08c425a4f193c00fe4b3
Aatraya23 <royaatraya7@gmail.com>
2024-05-12 23:06:52 +0530

    changes

--- Medical-Asst-System/resources/views/admin_panel/update.blade.php
+++ Medical-Asst-System/resources/views/admin_panel/update.blade.php
@@ -9,16 +9,18 @@
     <link rel="stylesheet" href="{{asset('css/amb_admin_reg.css')}}">
 </head>
 <body>
-<section class="container">
+<section class="container"> 
       <div class="title-bar">
         
         <header>Update Table Form</header>
       </div>
-      <form class="form" method="post" action="update_back.php"  enctype="multipart/form-data">
+      <form class="form" method="post" action="{{route('admin_panel.updated',['medical_supplies_medical'=>$medical_supplies_medical])}}"   enctype="multipart/form-data">
+           @csrf
+           @method('put')
         <div class="column">
           <div class="input-box">
             <label>Product Name <sub>(max 50 characters)</sub></label>
-            <input name="product_name" type="text" placeholder="Enter Product Name" maxlength="50" required/>
+            <input name="product_name" type="text" placeholder="Enter Product Name" maxlength="50" value="{{$medical_supplies_medical->product_name}}" required/>
           </div>
         </div>
         <div class="column">
@@ -30,21 +32,21 @@
         <div class="column">
           <div class="input-box">
             <label>E-mail</label>
-            <input name="email" type="text" placeholder="Enter email id" maxlength="50" required/>
+            <input name="email"  id="email" type="text" placeholder="Enter email id" maxlength="50" required/>
           </div>
         </div>
         <div class="column">
             <div class="input-box">
                 <label>Product Image </label>
-                <input name="photo" id="photo" type="file" placeholder="Enter Product Image" required />
+                <input name="image" id="photo" type="file" placeholder="Enter Product Image" required />
             </div>
 
             <div class="input-box">
                 <label>Table Name</label>
                 <div class="select-box">
-                    <select name="tabletype" id="">
-                        <option value="medical_supplies_medical">medical_supplies_medical</option>
-                        <option value="medical_supplies_technical">medical_supplies_technical</option>
+                    <select name="category" id="">
+                        <option value="medical">medical</option>
+                        <option value="technical">technical</option>
                     </select>
                 </div>
             </div>
@@ -52,24 +54,24 @@
         <div class="column">
           <div class="input-box">
             <label>Product ID</label>
-            <input name="product_id" type="number" placeholder="Enter Product Id" required/>
+            <input name="product_id" type="number" placeholder="Enter Product Id" value="{{$medical_supplies_medical->product_id}}" required/>
           </div>
           <div class="input-box">
             <label>Phone Number<sub>(without including +91)</sub</label>
-            <input name="drvcont" type="number" placeholder="Enter phone number" required maxlength="10"/>
+            <input name="drvcont" id="drvcont" type="number" placeholder="Enter phone number" required maxlength="10"/>
           </div>
         </div>
         <div class="column">
             <div class="input-box">
-                <label>Source ID</label>
-                <input name="source_id" type="number" placeholder="Enter Source Id" required maxlength="10"/>
+                <label>File name</label>
+                <input name="product_image" type="text" placeholder="Enter Source Id" required maxlength="10"/>
             </div>
         </div>
        
         <div class="column">
           <div class="input-box">
             <label>Product Info</label>
-            <input name="product_info" type="text" placeholder="Enter Product Info " required  minlength="148"  maxlength="150"/>
+            <input name="product_para" type="text" placeholder="Enter Product Info "required minlength="136"  maxlength="136"/>
           </div>
         </div>
         <div class="column">
@@ -81,7 +83,7 @@
         <div class="column">
           <div class="input-box">
             <label>Product Makers</label>
-            <input name="product_makers" type="text" placeholder="Enter Product Makers" required/>
+            <input name="product_makers" type="text" placeholder="Enter Product Makers"required/>
           </div>
         </div>
         <div class="column">
@@ -94,7 +96,7 @@
                 <input id="cnf-pswd"type="text" placeholder="Confirm your password" name ="pswd" required />
             </div>
         </div>
-        <button id="sbmt-form" name="submit" onclick="return confirm('Are you sure you want to update this product');">Update</button>
+        <button id="sbmt-form" name="submit" onclick="return confirm('Are you sure you want to insert this product');">Register</button>
       </form>
     </section>
     <script src="amb_admin_loc.js"></script>
