936c56610cd0719cffe2eb8823eeb60dd791e5eb
Aatraya23 <royaatraya7@gmail.com>
2024-05-12 17:52:11 +0530

    changes

--- Medical-Asst-System/vendor/composer/autoload_static.php
+++ Medical-Asst-System/vendor/composer/autoload_static.php
@@ -391,8 +391,8 @@ class ComposerStaticInit9b5903158699aa64ca9f77916b169812
         ),
         'Psr\\Http\\Message\\' => 
         array (
-            0 => __DIR__ . '/..' . '/psr/http-factory/src',
-            1 => __DIR__ . '/..' . '/psr/http-message/src',
+            0 => __DIR__ . '/..' . '/psr/http-message/src',
+            1 => __DIR__ . '/..' . '/psr/http-factory/src',
         ),
         'Psr\\Http\\Client\\' => 
         array (
--- Medical-Asst-System/vendor/composer/installed.php
+++ Medical-Asst-System/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'laravel/laravel',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => 'a5d92d225526449f2568bedd3609c443118cd9b0',
+        'reference' => 'e6b4c1b55dc65b8063b48f7b684c276ffed6aec2',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -400,7 +400,7 @@
         'laravel/laravel' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => 'a5d92d225526449f2568bedd3609c443118cd9b0',
+            'reference' => 'e6b4c1b55dc65b8063b48f7b684c276ffed6aec2',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
@@ -802,8 +802,8 @@
         'psr/log-implementation' => array(
             'dev_requirement' => false,
             'provided' => array(
-                0 => '3.0.0',
-                1 => '1.0|2.0|3.0',
+                0 => '1.0|2.0|3.0',
+                1 => '3.0.0',
             ),
         ),
         'psr/simple-cache' => array(
